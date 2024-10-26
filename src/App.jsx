523dc48a28599d7e84dc3dc2e3008d2705d2f877
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-26 14:17:24 +0500

    Set up a react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className='text-red-900'>Heloo</h1>
+    </>
+  )
+}
+
+export default App

249c7df2b4c415e2f9d79ec163accbe53b55b119
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-26 18:50:27 +0500

    Remove default styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <>
-     <h1 className='text-red-900'>Heloo</h1>
-    </>
-  )
+	return <></>;
 }
 
-export default App
+export default App;

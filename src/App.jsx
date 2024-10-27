14cb21e2fb0f2883f139ff7cdaf600e67c1a7630
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-27 10:48:42 +0500

    import Educationinfo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { PersonalInfo } from "./components/PersonalInfo";
+import { EducationalInfo } from "./components/EducationalInfo";
 
 function App() {
 	return (
 		<>
 			<PersonalInfo />
+			<EducationalInfo />
 		</>
 	);
 }

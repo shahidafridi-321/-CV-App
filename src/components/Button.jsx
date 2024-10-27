0e2720820f8a497c0e1bc2e18b611d365c5bb8a3
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-27 09:24:43 +0500

    Create Button componet

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export function Button({ text, classes, onClick }) {
+	return (
+		<button
+			className={classes + "border rounded-full py-2 px-8"}
+			onClick={onClick}
+		>
+			{text}
+		</button>
+	);
+}

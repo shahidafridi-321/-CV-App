9df7486036e777046e0ebced28f7beb17f7a757e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-28 15:36:27 +0500

    Remove un used import

--- src/components/EducationalInfo.jsx
+++ src/components/EducationalInfo.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "./Button";
 import { Input } from "./Input";
 import { Form } from "./Form";
 import { EducationalInfoCard } from "./EducationalInfoCard";

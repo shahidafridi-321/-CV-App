4229c63dd52d28f0fc067c3db7eba9ebcd8206c7
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-28 19:22:23 +0500

    Add hover effects to button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export function Button({ text, classes, onClick }) {
 	return (
 		<button
-			className={classes + " border rounded-full py-2 px-8"}
+			className={classes + " border rounded-full py-2  px-20 font-bold "}
 			onClick={onClick}
 		>
 			{text}

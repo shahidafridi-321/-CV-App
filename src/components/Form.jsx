6525b81ab635a3dc5fd62ab66b07c0d3d1bf37ab
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-28 19:24:14 +0500

    Add styles to form to look cool

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export const Form = ({ displayForm, children }) => {
+export const Form = ({ displayForm, classes = "", children ,title}) => {
 	return (
 		<form
 			className={
 				displayForm +
-				" md:flex flex-col justify-center align-middle md:mx-auto space-y-2  border-r border-black px-2"
+				" " +
+				classes +
+				"  font-mono  bg-white shadow-2xl rounded-xl flex-col justify-center align-middle md:flex  md:mx-auto space-y-2 md:px-9 md:py-9"
 			}
 		>
+			<h2 className="text-center font-light text-2xl font-serif">{title}</h2>
 			{children}
 		</form>
 	);

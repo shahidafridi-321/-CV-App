00ace06d5a00674346ac43d52762801672704d2e
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-30 08:50:59 +0500

    Remove Bg and add some cool styling

--- src/components/PersonalInfoCard.jsx
+++ src/components/PersonalInfoCard.jsx
@@ -8,11 +8,11 @@ export const PersonalInfoCard = ({
 	handleEdit,
 }) => {
 	return (
-		<div className=" md:flex md:flex-col md:justify-between md:col-span-3 md:text-center bg-gray-200 p-4">
+		<div className=" p-4 md:flex md:flex-col md:justify-between md:col-span-3 md:text-center  ">
 			<h1 className="md:text-8xl">{displayInfo.name}</h1>
-			<div className="flex justify-center space-x-6">
-				<p className="text-gray-900 text-2xl">{displayInfo.number}</p>
-				<p className="text-gray-900 text-2xl">{displayInfo.email}</p>
+			<div className="md:flex md:justify-center space-x-6">
+				<p className="md:text-gray-900 md:text-2xl">{displayInfo.number}</p>
+				<p className="md:text-gray-900 md:text-2xl">{displayInfo.email}</p>
 			</div>
 			<div>
 				{isSave ? (

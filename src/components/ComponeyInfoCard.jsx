ef1d904d410d380017cf08843fe60fb289d459e2
Shahid Afridi <shahidrahmansr87@gmail.com>
2024-10-30 08:48:18 +0500

    Add styling

--- src/components/ComponeyInfoCard.jsx
+++ src/components/ComponeyInfoCard.jsx
@@ -8,20 +8,32 @@ export const ComponeyInfoCard = ({
 	handleEdit,
 }) => {
 	return (
-		<div className="md:col-span-3 md:text-center">
-			<p>{displayComponeyInfo.componeyName}</p>
-			<p>{displayComponeyInfo.positionTitle}</p>
-			<p>{displayComponeyInfo.responsibilities}</p>
+		<div className=" p-4 md:-mt-[100px] md:flex md:flex-col md:gap-4 md:justify-start md:col-span-3 md:text-center">
+			<h2 className="md:text-4xl  text-gray-900">
+				{displayComponeyInfo.componeyName}
+			</h2>
+			<h3 className="md:text-2xl text-gray-800">
+				{displayComponeyInfo.positionTitle}
+			</h3>
+			<p className="md:text-xl text-gray-600">
+				{displayComponeyInfo.responsibilities}
+			</p>
 			<div className="flex flex-col md:flex-row space-x-2 justify-center">
 				<div className="flex space-x-1">
-					<span>Start Date </span>
-					<time dateTime={displayComponeyInfo.startDate}>
+					<span className="italic md:text-xl text-gray-600">Start Date </span>
+					<time
+						dateTime={displayComponeyInfo.startDate}
+						className="md:text-xl text-gray-600"
+					>
 						{displayComponeyInfo.startDate}
 					</time>
 				</div>
 				<div className="flex space-x-1">
-					<span> End Date </span>
-					<time dateTime={displayComponeyInfo.endDate}>
+					<span className="italic md:text-xl text-gray-600"> End Date </span>
+					<time
+						dateTime={displayComponeyInfo.endDate}
+						className="md:text-xl text-gray-600"
+					>
 						{displayComponeyInfo.endDate}
 					</time>
 				</div>
@@ -29,13 +41,13 @@ export const ComponeyInfoCard = ({
 			<div>
 				{isSave ? (
 					<Button
-						classes=" border-pink-600 "
+						classes=" border-pink-600 font-bold hover:bg-pink-600 hover:text-white "
 						onClick={handleSave}
 						text="Save"
 					/>
 				) : (
 					<Button
-						classes=" border-green-600 "
+						classes=" border-green-600  hover:bg-green-600 hover:text-white "
 						onClick={handleEdit}
 						text="Edit"
 					/>
